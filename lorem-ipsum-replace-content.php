26c3ceb10094bc6bcfb8fdae99cf9d51a01865c7
Evan Mullins <evanrm@gmail.com>
2021-07-13 19:07:40 -0400

    Update lorem-ipsum-replace-content.php
    cleanup unused functions and vars. update 'tested to'

--- lorem-ipsum-replace-content.php
+++ lorem-ipsum-replace-content.php
@@ -1,14 +1,20 @@
 <?php
-/*
-Plugin Name: Lorem Ipsum Replace Content
-Plugin URI: http://circlecube.com
-Description: Replace your content with lorem ipsum, word for word.
-Version: 0.3
-Author: Evan Mullins
-Author Email: evan@circlecube.com
-License: GPLv2 or later
+/**
+ * Plugin Name:       Lorem Ipsum Replace Content
+ * Plugin URI:        https://github.com/circlecube/lorem-ipsum-replace-content
+ * Description:       Render your content with lorem ipsum, word for word.
+ * Version:           0.4
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Evan Mullins
+ * Author URI:        https://circlecube.com
+ * License:           GPLv2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ */
 
-  Copyright 2011 Evan Mullins (evan@circlecube.com)
+/*
+
+  Copyright 2021 Evan Mullins (evan@circlecube.com)
 
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License, version 2, as 
@@ -27,60 +33,19 @@ License: GPLv2 or later
 
 class LoremIpsumReplaceContent {
 
-	/*--------------------------------------------*
-	 * Constants
-	 *--------------------------------------------*/
-	const name = 'Lorem Ipsum Replace Content';
-	const slug = 'lorem_ipsum_replace_content';
-	
 	/**
 	 * Constructor
 	 */
 	function __construct() {
-		//register an activation hook for the plugin
-		register_activation_hook( __FILE__, array( &$this, 'install_lorem_ipsum_replace_content' ) );
-
 		//Hook up to the init action
 		add_action( 'init', array( &$this, 'init_lorem_ipsum_replace_content' ) );
-	}
-  
-	/**
-	 * Runs when the plugin is activated
-	 */  
-	function install_lorem_ipsum_replace_content() {
-		// do not generate any output here
 	}
   
 	/**
 	 * Runs when the plugin is initialized
 	 */
 	function init_lorem_ipsum_replace_content() {
-		// Load JavaScript and stylesheets
-		$this->register_scripts_and_styles();
-
-	
-		if ( is_admin() ) {
-			//this will run when in the WordPress admin
-		} else {
-			//this will run when on the frontend
-		}
-
-		/*
-		 * TODO: Define custom functionality for your plugin here
-		 *
-		 * For more information: 
-		 * http://codex.wordpress.org/Plugin_API#Hooks.2C_Actions_and_Filters
-		 */
-		// add_action( 'your_action_here', array( &$this, 'action_callback_method_name' ) );
 		add_filter( 'the_content', array( &$this, 'lorem_ipsumize_the_content' ) );    
-	}
-
-	function action_callback_method_name() {
-		// TODO define your action method here
-	}
-
-	function filter_callback_method_name() {
-		// TODO define your filter method here
 	}
 
 	function lorem_ipsumize_the_content($content) {
@@ -136,42 +101,6 @@ class LoremIpsumReplaceContent {
 
 		return $content; 
 	}
-  
-	/**
-	 * Registers and enqueues stylesheets for the administration panel and the
-	 * public facing site.
-	 */
-	private function register_scripts_and_styles() {
-		if ( is_admin() ) {
-
-		} else {
-
-		} // end if/else
-	} // end register_scripts_and_styles
-	
-	/**
-	 * Helper function for registering and enqueueing scripts and styles.
-	 *
-	 * @name	The 	ID to register with WordPress
-	 * @file_path		The path to the actual file
-	 * @is_script		Optional argument for if the incoming file_path is a JavaScript source file.
-	 */
-	private function load_file( $name, $file_path, $is_script = false ) {
-
-		$url = plugins_url($file_path, __FILE__);
-		$file = plugin_dir_path(__FILE__) . $file_path;
-
-		if( file_exists( $file ) ) {
-			if( $is_script ) {
-				wp_register_script( $name, $url, array('jquery') ); //depends on jquery
-				wp_enqueue_script( $name );
-			} else {
-				wp_register_style( $name, $url );
-				wp_enqueue_style( $name );
-			} // end if
-		} // end if
-
-	} // end load_file
   
 } // end class
 new LoremIpsumReplaceContent();
